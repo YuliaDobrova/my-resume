b6e5985276c5b6147ef35a8b72c2976a5d862565
Yulia Dobrova <dobrova2009@gmail.com>
2024-01-29 01:02:00 -0800

    scroll added

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+/** Scroll to top button **/
+
+let intervalId = 0;
+const $scrollButton = document.querySelector('.scroll');
+
+function scrollStep() {
+  if (window.pageYOffset === 0) {
+    clearInterval(intervalId);
+  }
+  window.scroll(0, window.pageYOffset - 50);
+}
+
+function scrollToTop() {
+  intervalId = setInterval(scrollStep, 16.66);
+}
+
+$scrollButton.addEventListener('click', scrollToTop);
